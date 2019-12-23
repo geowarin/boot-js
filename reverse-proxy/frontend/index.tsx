02df9c9c16d493b2f0fa43af110e7e8fadf9eed0
Geoffroy Warin <code@geowarin.com>
2019-12-23 20:30:55 +0100

    Reverse-proxy

--- reverse-proxy/frontend/index.tsx
+++ reverse-proxy/frontend/index.tsx
@@ -2,13 +2,37 @@ import * as React from "react";
 import {render} from "react-dom";
 import wretch from "wretch";
 import {useAsync} from "react-async-hook";
+import {Link, Route} from "wouter";
+
+const RestPage = () => {
+  const {error, result, loading} = useAsync<{ message: string }>(async () => wretch().url("/api/message").get().text(), []);
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>failed to load {error.message}</div>;
+
+  return (
+    <>
+      <p>Message from server: {result} !!</p>
+      <Link to="/other">Go to the other page</Link>
+    </>
+  );
+};
+
+const OtherPage = () => {
+  return (
+    <>
+      <h2>Ho hai!</h2>
+      <Link to="/">Go to the home page</Link>
+    </>
+  )
+};
 
 const App = () => {
-  const state = useAsync<{ message: string }>(async () => wretch().url("/api/message").get().json(), []);
-
-  if (state.loading) return <div>loading...</div>;
-  if (state.error) return <div>failed to load</div>;
-  return <div>Message from server: {state.result?.message} !!</div>;
+  return (
+    <>
+      <Route path="/" component={RestPage}/>
+      <Route path="/other" component={OtherPage}/>
+    </>
+  )
 };
 
 const rootElement = document.getElementById("root");
